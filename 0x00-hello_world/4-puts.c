41bd4a0ebe94c0d8333f9f5ab5fd353152212207
fletcher429 <davifletcher2003@gmail.com>
2023-03-16 10:20:46 -0700

    Betty why me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * main - this is the main function
+ *
+ * Return always 0
+ */
+
+int main() 
+{
     puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
