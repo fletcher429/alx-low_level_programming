aa518e2d047f3d7e794fad663a9884044c66e6ad
fletcher429 <davifletcher2003@gmail.com>
2023-03-17 10:20:14 -0700

    If else While

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when done
+ */
+
+
+int main(void)
+{
+
+	int n;
+
+	strand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/**
+	 * This is where my code will go
+	 *
+	 *
+	 * Happy coding
+	 */
+
+	if (n > 5)
+	{
+		printf("is %d and is greater than 5\n", n);
+	}
+	else
+	{
+		if (n == 0)
+		{
+			printf("is %d and is 0\n", n);
+		}
+	}
+	else
+	{
+		printf("is %d and is less than 6 and not 0\n", n);
+	}
+}
