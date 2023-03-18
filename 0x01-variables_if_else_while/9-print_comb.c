1ab6cbfbd55a83446c2cf2ef40a91f0ee3f1a85d
fletcher429 <davifletcher2003@gmail.com>
2023-03-18 04:51:11 -0700

    Digit Combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - this the main function
+ *
+ * Return: 0 when done;
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+
+	if (num < 48)
+	{
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
+
+
+
+
+
+
+}
