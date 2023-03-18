dcea9143136b7567d87a99835923114021e33e21
fletcher429 <davifletcher2003@gmail.com>
2023-03-18 04:33:02 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when done
+ */
+
+
+int main(void)
+{
+	int n;
+	int x;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (x = 65; x <= 70; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
