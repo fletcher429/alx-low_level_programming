6a3e244234a9627911c2148650deb57dd06ecbc3
fletcher429 <davifletcher2003@gmail.com>
2023-03-18 03:44:32 -0700

    I love Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ *
+ * Return: 0 when done
+ */
+
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+
+	}
+}
