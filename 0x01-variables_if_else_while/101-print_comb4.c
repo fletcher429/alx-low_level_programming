4788b931f593466de7d9f33980ad90eeb4331b13
fletcher429 <davifletcher2003@gmail.com>
2023-03-18 07:26:03 -0700

    $ digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,16 +12,16 @@ int main(void)
 {
 	int a, b, c, count = 0;
 
-	for (a = 0; a < 7; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (b = a + 1; b < 8; b++)
+		for (b = a + 1; b < 10; b++)
 		{
-			for (c = b + 1; c < 9; c++)
+			for (c = b + 1; c < 10; c++)
 			{
 				putchar('0' + a);
 				putchar('0' + b);
 				putchar('0' + c);
-				putchar(count < 83 ? ',' : ' ');
+				putchar(count < 119 ? ',' : ' ');
 				putchar(32);
 				count++;
 
