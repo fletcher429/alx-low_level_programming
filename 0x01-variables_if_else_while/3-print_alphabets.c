ac791c5b37ca12cd42332d1c9b6a2ec6e9663df2
fletcher429 <davifletcher2003@gmail.com>
2023-03-18 03:12:59 -0700

    alphaBEt

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * short description
+ * Return :0 when done
+ */
+
+int main(void)
+{
+	int a;
+	int Z;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	for (Z = 65; Z <= 90; Z++)
+	{
+		putchar(Z);
+	}
+	putchar('\n');
+	return (0);
+}
