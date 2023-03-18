2327b514d0f315b85757a4e143d56e7f9fa62eab
fletcher429 <davifletcher2003@gmail.com>
2023-03-18 04:20:14 -0700

    Alphabet in Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when done;
+ *
+ */
+
+int main(void)
+{
+	char n = 'z';
+
+	while (n >= 'a')
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+
+
+
+
+
+
+
+
+}
