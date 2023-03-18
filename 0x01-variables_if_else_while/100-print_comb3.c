7bbbf8762f9d8aaea56972e9439c6d64890395df
fletcher429 <davifletcher2003@gmail.com>
2023-03-18 05:36:21 -0700

    Two Gs

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main functin
+ *
+ *
+ * Return: 0 when done
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 10; n <= 90; n++)
+	{
+		putchar('0' + n / 10);
+		putchar('0' + n % 10);
+		putchar(44);
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
