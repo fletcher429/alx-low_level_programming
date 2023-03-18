8379070796410f0b001a71f47e956506f6c0aab1
fletcher429 <davifletcher2003@gmail.com>
2023-03-18 04:04:02 -0700

    Single digit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ *
+ * Return: 0 when done
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+
+
+
+
+
+
+
+
+
+
+}
