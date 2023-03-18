bf0fb2a932a166c9fdd952d35ae363c2e2aee8ba
fletcher429 <davifletcher2003@gmail.com>
2023-03-18 05:09:01 -0700

    9-combi

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,12 +13,11 @@ int main(void)
 	for (num = 48; num <= 57; num++)
 	{
 		putchar(num);
-	}
-
-	if (num < 57)
-	{
-		putchar(44);
-		putchar(32);
+		if (num < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
 	putchar('\n');
 	return (0);
