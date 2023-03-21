7e971f2ff77a0337aefab87ee7ffd128cec11365
fletcher429 <davifletcher2003@gmail.com>
2023-03-21 12:04:43 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main funct
+ *
+ *
+ *
+ * Return: 0 when done
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
+
+
+
+}
