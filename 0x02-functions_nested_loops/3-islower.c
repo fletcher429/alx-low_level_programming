f68e1714dd619c6e95dcc94f677646d8306c618a
fletcher429 <davifletcher2003@gmail.com>
2023-03-21 14:34:32 -0700

    _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _islower - gets the lowercase letters
+ *
+ * @c: ASCII code for letters
+ *
+ *
+ * Return: 0 when false
+ */
+
+int _islower(int c)
+
+{
+	iint c;
+
+	if (c == 97 || c == 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
