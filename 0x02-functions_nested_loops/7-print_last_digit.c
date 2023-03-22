c785c7fde673eec4753695f90bf434c75575a54e
fletcher429 <davifletcher2003@gmail.com>
2023-03-22 02:16:17 -0700

    The last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints every minute in day
+ * @a: gets the last digit
+ *
+ *
+ * Return: 0 when gotten
+ *
+ */
+int print_last_digit(int a)
+{
+	a = a % 10;
+	_putchar(a);
+	return (0);
+}
