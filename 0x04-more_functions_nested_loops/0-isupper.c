9fbda9757245e21c9a6c783fe1010c9d078fd0e9
fletcher429 <davifletcher2003@gmail.com>
2023-03-23 07:40:45 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - gets the uppercase
+ *
+ * Return: 1 if true
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
