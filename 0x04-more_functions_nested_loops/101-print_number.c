cddaf133a384edddf1a76c05e9df4ab725f9f55e
fletcher429 <70367692+fletcher429@users.noreply.github.com>
2023-03-24 23:37:17 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,4 +4,34 @@
  * print_number - prints an integer
  * @n: integer to be printed
  */
+void print_number(int n)
+{
+    int digit, power, sign;
+
+    if (n == 0) 
+    {
+        putchar('0');
+        return;
+    }
+
+    sign = n < 0 ? -1 : 1;
+    n *= sign;
+
+    power = 1;
+    while (n / power >= 10) {
+        power *= 10;
+    }
+
+    if (sign == -1) {
+        putchar('-');
+    }
+
+    while (power > 0) {
+        digit = n / power;
+        putchar(digit + '0');
+        n -= digit * power;
+        power /= 10;
+    }
+}
+
 
