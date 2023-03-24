936497068f6217482aad386f8f51f5bfe0be6999
fletcher429 <davifletcher2003@gmail.com>
2023-03-24 10:53:33 -0700

    syntax fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,14 +12,14 @@ void more_numbers(void)
 	int a;
 	int b;
 
-	for (a = 1; a <= 10, a++)
+	for (a = 1; a <= 10; a++)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			if (j >= 10)
+			if ( b>= 10)
 			{
 				_putchar('1');
-				_putchar(j % 10 + '0');
+				_putchar(b % 10 + '0');
 			}
 		}
 		_putchar('\n');
