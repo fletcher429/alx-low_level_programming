fcd15e36ec517a145c610ace270cd10b7a72e6ab
fletcher429 <davifletcher2003@gmail.com>
2023-03-27 11:10:19 -0700

    stringy

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: cool
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i ;
+
+	for (i = 0; i[str] != '\0'; i++)
+	{
+		_puts(i[str]);
+	}
+	putchar('\n');
+}
