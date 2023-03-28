e57f1b323a35b0a7dd3aab0688e236d88d87c891
fletcher429 <davifletcher2003@gmail.com>
2023-03-28 06:07:17 -0700

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,15 +3,20 @@
  * print_rev - prints char in reverse
  * @s: is char type var
  * @i: iteration
+ * @j: while
  * Return: void
  */
 void print_rev(char *s)
 {
-	int i;
+	int i, j, a;
 
-	for (i = 5; i[s] = '\0'; i--)
+	for (i = 0; s[i] != '\0'; i++);
+
+	j = i;
+
+	for (a = j - 1; a => 0; i--)
 	{
-		_putchar(i);
+		_putchar(s[a]);
 	}
 	_putchar('\n');
 }
