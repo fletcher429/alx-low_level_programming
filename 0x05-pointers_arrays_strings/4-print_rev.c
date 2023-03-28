11cf919b170d1730f036073b4da94f87c2e306b8
fletcher429 <davifletcher2003@gmail.com>
2023-03-28 05:50:34 -0700

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints char in reverse
+ * @s: is char type var
+ * @i: iteration
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 5; i[s] = '\0'; i--)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
