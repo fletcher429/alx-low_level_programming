89d6dadc6a3cfbfac10e9f7654091261d1976dfa
fletcher429 <davifletcher2003@gmail.com>
2023-04-05 07:00:07 -0700

    the value of power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <math.h>
+/**
+ * _pow_recursion - returns the pow of y
+ * @x: value to be returned
+ * @y: the power
+ * Return: returns the pow of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return x  * _pow_recursion(x, y-1);
+	}
+}
