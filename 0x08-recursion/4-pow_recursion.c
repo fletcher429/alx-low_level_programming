ce40ac37597afe958a605001d2548bfa7669cacf
fletcher429 <davifletcher2003@gmail.com>
2023-04-05 07:30:54 -0700

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,8 +12,19 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
+	else if (y % 2 == 0)
+	{
+		int hlf = _pow_recursion(x, y / 2);
+
+		return (x * hlf * hlf);
+	}
 	else
 	{
-		return x  * _pow_recursion(x, y);
+		int hlf = _pow_recursion(x, (y - 1) / 2);
+
+		return (x * hlf * hlf);
 	}
+	int result = _pow_recursion(x, y);
+
+	return (result);
 }
