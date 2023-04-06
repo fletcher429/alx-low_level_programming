3dba9985ee32e49c04f32cb5b4ebef85af087dfd
fletcher429 <davifletcher2003@gmail.com>
2023-04-06 07:30:09 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,41 +1,40 @@
 #include "main.h"
 /**
  * _sqrt_recursion -  the recursive func
- * sqrt_algo_func - finds the squareroot
+ * culc_sqrt - finds the squareroot
  * @n: squareroot of number
  * @i: sets the iteration
  * Return: squareroot of a natural number
  */
-int sqrt_algo_func(int n, int i);
 
-
-
-int _sqrt_recursion(int n)
+int culc_sqrt(int i, int n)
 {
-
 	if (n < 0)
 	{
 		return (-1);
 	}
-	else
+	else if (n > 0)
 	{
-		return (sqrt_algo_func(n, 0));
-	}
-}
-
-int sqrt_algo_func(int n, int i)
-{
-
-	if (i * i > n)
-	{
-		return (-1);
-	}
-	else if (i * i == n)
-	{
-		return (sqrt_algo_func(n, i + 1));
+		if (i * i > n)
+		{
+			return (-1);
+		}
+		else if (i * i == n)
+		{
+			return (i);
+		}
+		else
+		{
+			return (culc_sqrt(n, i + 1));
+		}
 	}
 	else
 	{
 		return (0);
 	}
+}
+
+int int _sqrt_recursion(int n)
+{
+	return (culc_sqrt(1, n));
 }
