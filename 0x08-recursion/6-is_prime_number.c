4fed456a284cf8f7801e068dfecad2c219432289
fletcher429 <davifletcher2003@gmail.com>
2023-04-06 13:52:24 -0700

    doc fixed

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,14 +11,19 @@ int prime_number(int i, int n)
 {
 	if (i * i > n)
 		return (1);
-	if(n %  i == 0 && i > 0)
+	if (n %  i == 0 && i > 0)
 		return (0);
-	return (prime_number(i -1, n));
+	return (prime_number(i - 1, n));
 }
+/**
+ * is_prime_number - calls the helper
+ * @n: prime number
+ * Return: prime
+ */
 
 int is_prime_number(int n)
 {
 	if (n < 2)
 		return (0);
-	return (prime_number(n, n - 1));	
+	return (prime_number(n, n - 1));
 }
