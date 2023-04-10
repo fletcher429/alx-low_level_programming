849a46843aae430718daac5a16b77417a12ea241
fletcher429 <davifletcher2003@gmail.com>
2023-04-10 10:09:38 -0700

    prints commandline args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**main - prints all arguments passed to it
+ * @argv: pointers arrays of command line argument
+ * @argc: no of command - line arguments
+ */
+int main (int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; argv[i] != NULL; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	putchar('\n');
+}
