04f32771bf794265454636f696c9af7caed2a242
fletcher429 <davifletcher2003@gmail.com>
2023-04-10 14:06:36 -0700

    adds two positive number

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - prints the adds of two integer
+ * @argv: points to the arrays of string
+ * @argc: no of command line args
+ * Return: void
+ */
+
+int main (int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		int x;
+
+		for (x = 0; i < argv[i][x]; x++)
+		{
+			if (!isdigit(argv[i][x]))
+			{
+				printf ("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+
+	}
+	printf("%d\n", sum);
+	return (0);
+}
---
+++ 4-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the sum of two positive numbers
+ * @argv: ponters to arrays of string
+ * @argc: number command line argument
+ * Return: void
+ */
+int main (int argc , char *argv[])
+{
+
+}
