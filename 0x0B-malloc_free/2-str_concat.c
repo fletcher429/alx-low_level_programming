e9585bc518746a3edff27ca80a68d816d7480d5b
Davidomondi <davifletcher2003@gmail.com>
2023-04-12 08:13:45 -0700

    fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,28 +9,29 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned len1, len2, i;
+	unsigned int len1, len2, i;
 	char *concat;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	
-	for (len1 = 0; s1[len1] != '\0'; len1++);
-	for (len2 = 0; s2[len2] != '\0'; len2++);
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
 
 	concat = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (concat == NULL)
 		return (NULL);
-	
 	for (i = 0; s1[i] != '\0'; i++)
+	{
 		concat[i] = s1[i];
+	}
 	for (i = 0; s2[i] != '\0'; i++)
+	{
 		concat[len1 + i] = s2[i];
-	
+	}
 	concat[len1 + len2] = '\0';
-
 	return (concat);
-	
 }
