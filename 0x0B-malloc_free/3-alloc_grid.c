ffcd783dac549e46649ec89d0db02cdf730e918e
Davidomondi <davifletcher2003@gmail.com>
2023-04-12 13:04:16 -0700

    alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,29 +10,30 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j;
-	int **two_d;
+	int **two;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	two_d = (int **)malloc(sizeof(int *) * height);
+	two = malloc(sizeof(int) * height);
 
-	if (two_d == NULL)
+	if (two == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		two_d[i] = (int *) malloc(sizeof(int) * width);
+		two[i] = malloc(sizeof(int) * width);
 
-		if (two_d == NULL)
+		if (two[i] == NULL)
 		{
 			for (j = 0; j < i; i++)
-				free(two_d[j]);
-			free(two_d);
+				free(two[j]);
+			free(two);
 			return (NULL);
 		}
-		for (j = 0; j < width; j++)
-			two_d[i][j] = 0;
 	}
-	return (two_d);
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			two[i][j] = 0;
+	return (two);
 }
