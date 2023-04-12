1f67692fbf78e26432e09eac9bd9e423a689ebbf
Davidomondi <davifletcher2003@gmail.com>
2023-04-12 07:57:48 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,20 +1,36 @@
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
-char *str_concat(const char *s1, const char *s2) {
-    if (!s1) s1 = "";
-    if (!s2) s2 = "";
+/**
+ * str_concat - concate two stings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: concat_string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned len1, len2, i;
+	char *concat;
 
-    size_t len1 = strlen(s1);
-    size_t len2 = strlen(s2);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	
+	for (len1 = 0; s1[len1] != '\0'; len1++);
+	for (len2 = 0; s2[len2] != '\0'; len2++);
 
-    char *result = malloc(len1 + len2 + 1);
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (concat == NULL)
+		return (NULL);
+	
+	for (i = 0; s1[i] != '\0'; i++)
+		concat[i] = s1[i];
+	for (i = 0; s2[i] != '\0'; i++)
+		concat[i] = s2[i];
+	
+	concat[i] = '\0';
 
-    if (result) {
-        memcpy(result, s1, len1);
-        memcpy(result + len1, s2, len2 + 1);
-    }
-
-    return result;
+	return (concat);
+	
 }
-
