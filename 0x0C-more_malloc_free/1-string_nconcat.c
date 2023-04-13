463d9fcfbcbc60ac03c3f0e7b020ebcca6ede27d
Davidomondi <davifletcher2003@gmail.com>
2023-04-13 13:28:14 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,48 @@
-#include "main.h" 
-#include <stdlib.h> 
-  
- /** 
-  * string_nconcat - concates two strings 
-  * @s1: string 1 
-  * @s2: string 2 
-  * @n: number of bytes of s2 
-  * Return: new string 
-  */ 
- char *string_nconcat(char *s1, char *s2, unsigned int n) 
- { 
-         unsigned int i, j, l1 = 0, l2 = 0; 
-         char *concat; 
-  
-         for (i = 0; s1 && s1[i]; i++) 
-                 l1++; 
-         for (j = 0; s2 && s2[i]; i++) 
-                 l2++; 
-         if (n < l2) 
-         { 
-                 concat = malloc(sizeof(char) *(l1 + n + 1)); 
-         } 
-         else 
-         { 
-                 concat = malloc(sizeof(char) * (l1 + l2 + 1)); 
-         } 
-         if (!concat) 
-         { 
-                 return (NULL); 
-         } 
-         for (i = 0; i < l1; i++) 
-                 concat[i] = s1[i]; 
-         for (j = 0; n < l2 &&  i < (l1 + n); i++, j++) 
-                 concat[j] = s2[j]; 
-         for (j = 0; n >= l2 && i < (l1 + l2); i++, j++) 
-                 concat[i] = s2[j]; 
-         concat[i] = '\0'; 
-  
-         return (concat); 
-  
-  
-  
-  
-  
-  
-  
-  
- }
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strlen - finds the legnth of a string
+ * @length - the string
+ * Return: legnth of a string
+ */
+int strlen(char *length)
+{
+	int j;
+
+	for (j = 0; length[j] != '\0'; j++)
+		;
+	return (j);
+}
+
+/**
+ * string_nconcat - concates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of bytes of s2 to new string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, l1, l2;
+	char *cont;
+	
+       if (s1 == NULL)
+	       s1 = "";
+       if (s2 == NULL)
+	       s2 = "";
+       if (n < strlen(s2))
+	       strlen(s2) = num;
+       l1 = strlen(s1) + n + 1;
+
+       count = malloc(sizeof(*count) * l1);
+       
+       if(!count)
+	       return (NULL);
+
+       for (i = 0; s1[i] != '\0'; i++)
+	       cont[i] = s1[i];
+       for (j = 0; j < n; j++)
+	       cont[i + j] = s2[j];
+       cont[i + j] ='\0';
+
+       return(cont);
+}
