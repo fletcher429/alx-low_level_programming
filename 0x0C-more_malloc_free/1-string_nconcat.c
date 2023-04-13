2faf29d20b6284a4fd0c97c39804d46d37cd7874
DAVID OMONDI <70367692+fletcher429@users.noreply.github.com>
2023-04-13 20:46:40 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,49 @@
-#include "main.h"
-#include <stdlib.h>
-
-/**
- * string_nconcat - concates two strings
- * @s1: string 1
- * @s2: string 2
- * @n: number of bytes of s2
- * Return: new string
- */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	unsigned int i, j, l1 = 0, l2 = 0;
-	char *concat;
-        if (n < l2) 
+#include "main.h" 
+ #include <stdlib.h> 
+  
+ /** 
+  * string_nconcat - concates two strings 
+  * @s1: string 1 
+  * @s2: string 2 
+  * @n: number of bytes of s2 
+  * Return: new string 
+  */ 
+ char *string_nconcat(char *s1, char *s2, unsigned int n) 
+ { 
+         unsigned int i, j, l1 = 0, l2 = 0; 
+         char *concat; 
+  
+         for (i = 0; s1 && s1[i]; i++) 
+                 l1++; 
+         for (j = 0; s2 && s2[i]; i++) 
+                 l2++; 
+         if (n < l2) 
          { 
                  concat = malloc(sizeof(char) *(l1 + n + 1)); 
          } 
          else 
          { 
-                 concat = malloc(sizeof(char) * (l1 + l2 + 1));
-
-	for (i = 0; s1 && s1[i]; i++)
-		l1++;
-	for (j = 0; s2 && s2[i]; i++)
-		l2++;
-	if (!concat)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < l1; i++)
-		concat[i] = s1[i];
-	for (j = 0; n < l2 &&  i < (l1 + n); i++, j++)
-		concat[j] = s2[j];
-	for (j = 0; n >= l2 && i < (l1 + l2); i++, j++)
-		concat[i] = s2[j];
-	concat[i] = '\0';
-
-	return (concat);
-
-
-
-
-
-
-
-
-}
+                 concat = malloc(sizeof(char) * (l1 + l2 + 1)); 
+         } 
+         if (!concat) 
+         { 
+                 return (NULL); 
+         } 
+         for (i = 0; i < l1; i++) 
+                 concat[i] = s1[i]; 
+         for (j = 0; n < l2 &&  i < (l1 + n); i++, j++) 
+                 concat[j] = s2[j]; 
+         for (j = 0; n >= l2 && i < (l1 + l2); i++, j++) 
+                 concat[i] = s2[j]; 
+         concat[i] = '\0'; 
+  
+         return (concat); 
+  
+  
+  
+  
+  
+  
+  
+  
+ }
