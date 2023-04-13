aafb79c1dad7dc81318d82187e9494aae7bf7604
Davidomondi <davifletcher2003@gmail.com>
2023-04-13 04:20:32 -0700

    creates a pointer

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: size of mem
+ * Return: pointer allocated
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *s;
+
+	s = (int *) malloc(sizeof(int) * b);
+
+	if (s == NULL)
+	{
+		exit (98);
+	}
+	return (s);
+}
