e41be7536bea8ffdf9b0bbf05539a62c7f099ca1
Davidomondi <davifletcher2003@gmail.com>
2023-04-14 03:23:23 -0700

    typo

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,8 +11,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *ptr;
-	unsigned int i, total_size;
-	size = sizeof(int);
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
