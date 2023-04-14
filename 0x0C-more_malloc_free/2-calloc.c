2faffb458d9f9123729163c1ef353fcdccdc9bf1
Davidomondi <davifletcher2003@gmail.com>
2023-04-14 03:29:43 -0700

    total_size

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,15 +11,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
-	unsigned int i;
+	unsigned int i, total_size;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(nmemb * size);
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
 	if (!ptr)
 		return (NULL);
 
-	for (i = 0; i < (nmemb * size); i++)
+	for (i = 0; i < total_size; i++)
 		ptr[i] = 0;
 
 	return (ptr);
