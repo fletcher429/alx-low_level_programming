925a672c5a027b2910d9ea82260fca3f17fd0000
Davidomondi <davifletcher2003@gmail.com>
2023-04-17 04:15:18 -0700

    typo

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,10 +13,10 @@
 int print_details(struct dog *d)
 {
 	if (d == NULL)
-		return (68990);
+		return;
 	if (d->name == NULL)
-		d->name = "(Nil)";
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->owner = "(Nil)";
+		d->owner = "(nil)";
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
