84f16ceff6cc50edbe5c6d45ab6edef610c2e667
Davidomondi <davifletcher2003@gmail.com>
2023-04-17 03:16:11 -0700

    typo

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,22 +11,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d = malloc(sizeof(struct dog));
+	if(!d)
+		return  (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
 
 
-}
-/**
- * main - calls the function
- *
- * Return: 0
- */
-
-int main(void)
-{
-	struct dog my_dog;
-
-	init_dog(&my_dog, "Poppy", 3.5, "Bob");
-	return (0);
 }
