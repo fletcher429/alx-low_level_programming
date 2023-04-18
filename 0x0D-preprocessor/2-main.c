6cdc24f45bea4f698c357524e89dae0e5c807270
Davidomondi <davifletcher2003@gmail.com>
2023-04-17 19:09:15 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of thne programm
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
