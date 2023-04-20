48e27b791b61fcf74f0675052464cbc8b943343a
Davidomondi <davifletcher2003@gmail.com>
2023-04-20 07:26:05 -0700

    done

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -25,9 +25,9 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (ops[i].op == NULL)
+	while (ops[i].op != 0)
 	{
-		if (strcmp(ops[i].op), s == 0)
+		if (strcmp(ops[i].op, s) == 0)
 		{
 			return (ops[i].f);
 		}
