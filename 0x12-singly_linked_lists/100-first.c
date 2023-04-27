526830a848f75f70531f5d5041cdacc465991a07
Davidomondi <davifletcher2003@gmail.com>
2023-04-27 12:38:57 -0700

    desc

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,10 +7,10 @@
  *
  */
 
-void print_first(void)__attribute__ ((constructor));
+int print_first(void)__attribute__ ((constructor));
 
 
-void print_first(void)
+int print_first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -12,7 +12,7 @@
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description: singly linked list node structure is amazing
  */
 typedef struct list_s
 {
@@ -25,6 +25,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void print_first(void);
+int print_first(void);
 
 #endif
