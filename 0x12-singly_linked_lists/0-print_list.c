cab86131f1d2de623033105fd4e2119a708c3234
David <davifletcher2003@gmail.com>
2023-04-27 16:17:12 +0300

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+
+#include "main.h"
+/**
+ * print_list - prints list
+ * @h: head
+ * 
+*/
+
+size_t print_list(const list_t *h)
+{
+    size_t count = 0;
+    while (h)
+    {
+        if (h->str == NULL)
+        {
+            printf("[0] (nil)\n");
+        }
+        else
+        {
+            printf("[%u] %s\n", h->len, h->str);
+        }
+        count++;
+        h = h->next;
+
+    }
+    return (count);
+}
