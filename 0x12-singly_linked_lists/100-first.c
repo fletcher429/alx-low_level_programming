57023cc1c867ec93732195b4854d6afa5719d829
Davidomondi <davifletcher2003@gmail.com>
2023-04-27 12:10:14 -0700

    print_befor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_first - prints beforee main
+ *
+ * Return: void;
+ *
+ */
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+int main(void)
+{
+	print_first();
+
+
+	return (0);
+}
