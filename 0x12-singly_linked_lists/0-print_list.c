014d7800d1bc55b3803817ace98864b713dfc936
Davidomondi <davifletcher2003@gmail.com>
2023-04-27 06:50:05 -0700

    betty_linter

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,25 +2,24 @@
 /**
  * print_list - prints list
  * @h: head
- * 
+ * Return: count
+ *
 */
 
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
-    while (h)
-    {
-        if (h->str == NULL)
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%u] %s\n", h->len, h->str);
-        }
-        count++;
-        h = h->next;
+	size_t  count = 0;
 
-    }
-    return (count);
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)"\n);
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
