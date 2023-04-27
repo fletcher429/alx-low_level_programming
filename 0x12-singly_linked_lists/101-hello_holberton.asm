ce0a2877b3f19ba7accc4749422e92c6416ff2a4
Davidomondi <davifletcher2003@gmail.com>
2023-04-27 13:36:20 -0700

    assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,16 @@
-section .data
-    hello db 'Hello, Holberton', 0xA ; message to be printed
+global main
 
 section .text
-    global _start
+main:
+	mov rax, 1;write(
+	move rdi,1;STDOUT_FILNO,
+	mov rsi, msg;"Hello, Holberton\n",
+	mov rdx, msglen;sizeof("Hello, Holberton\n")
+		syscall;);
+	mov rax, 60; exit(
+	mov rdi, 0;EXIT_SUCESS
+	sycall;);
 
-_start:
-    ; write the message to stdout
-    mov eax, 4      ; system call for write
-    mov ebx, 1      ; file descriptor for stdout
-    mov ecx, hello  ; address of message to be printed
-    mov edx, 16     ; length of message
-    int 0x80        ; call kernel
-
-    ; exit the program
-    mov eax, 1      ; system call for exit
-    xor ebx, ebx    ; exit status code (0)
-    int 0x80        ; call kernel
-
+section .data
+	msg:db"Hello, Holberton", 10
+	msglen:qu$ - msg
