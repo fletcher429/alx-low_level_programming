a2150b5071d07e2b80e5575cddb07e9e38e74039
Davidomondi <davifletcher2003@gmail.com>
2023-04-27 12:27:43 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,15 +6,13 @@
  * Return: void;
  *
  */
+
+void print_first(void)__attribute__ ((constructor));
+
+
 void print_first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-int main(void)
-{
-	print_first();
 
-
-	return (0);
-}
