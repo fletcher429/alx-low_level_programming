4747e2965a2b4fd090e824d7a79188092e9375ca
Davidomondi <davifletcher2003@gmail.com>
2023-04-27 13:48:30 -0700

    assemble

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,15 +2,15 @@ global main
 
 section .text
 main:
-	mov rax, 1;write(
-	move rdi,1;STDOUT_FILNO,
-	mov rsi, msg;"Hello, Holberton\n",
-	mov rdx, msglen;sizeof("Hello, Holberton\n")
-		syscall;);
-	mov rax, 60; exit(
-	mov rdi, 0;EXIT_SUCESS
-	sycall;);
+	mov rax, 1	; write(
+	mov rdi, 1	; STDOUT_FILNO,
+	mov rsi, msg	; "Hello, Holberton\n",
+	mov rdx, msglen	; sizeof("Hello, Holberton\n")
+	syscall		; );
+	mov rax, 60	; exit(
+	mov rdi, 0	; EXIT_SUCESS
+	syscall		; );
 
 section .data
-	msg:db"Hello, Holberton", 10
-	msglen:qu$ - msg
+	msg:	db "Hello, Holberton", 10
+	msglen:	equ $ - msg
