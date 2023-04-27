6019a3a7761627d93e843d04034b0c3e878114ea
Davidomondi <davifletcher2003@gmail.com>
2023-04-27 11:21:24 -0700

    lol

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -6,9 +6,10 @@
  * @str: pointer of string added
  * Return: new node
  */
-list_t *add_node_end(list_t **head, const char *str)	
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *current;
+	list_t *new;
+	list_t *current = *head;
 	int len = 0;
 
 	while (str[len])
@@ -36,7 +37,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		current = *head;
-		while (current->next != NULL)
+		while (current->next)
 		{
 			current = current->next;
 		}
