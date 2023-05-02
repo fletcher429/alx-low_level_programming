ad2209420690aa202c213dd23b4ec45b72bcf769
Davidomondi <davifletcher2003@gmail.com>
2023-05-02 14:31:38 -0700

    c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		temp = temp->next;
 	}
 	if (temp == NULL)
-		return -1;
+		return (-1);
 	prev->next = temp->next;
 	free(temp);
 	return (1);
