3cac3b6b813b3f8d5eb38bb235c2d1edf4e4d5e9
David <davifletcher2003@gmail.com>
2023-05-05 00:24:51 +0300

    task_1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,11 +18,12 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
+	while(b[i])
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		result = 2 * result + (b[i] - '0');
+		i++;
 	}
 	return (result);
 }
