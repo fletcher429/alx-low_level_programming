e645a04c220d3086c03aaaa3aa94cab2e664ec00
David <davifletcher2003@gmail.com>
2023-05-05 22:16:33 +0300

    fix_it

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,10 +8,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL || index > 64)
+	if (n == NULL || index > 63)
 		return (-1);
 
-	*n &= ~(1 << index);
+	*n &= ~(1UL << index);
 
-	return ((*n >> index) & 1);
+	return (1);
 }
