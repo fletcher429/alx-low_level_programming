5394fe9b8b54e7841ba1de556bff16cc9028fdb3
David <davifletcher2003@gmail.com>
2023-05-05 11:52:28 +0300

    debug

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,18 +8,21 @@
 
 void print_binary(unsigned long int n)
 {
-	char bin[32] = {0};
-	int j;
-	int i = 0;
+	int bin = 0;
+	int base = 1;
 
 	if (n == 0)
-		_putchar('0');
-	while (n > 0)
 	{
-		bin[i++] = (n & 1) + ('0');
-		n >>= 1;
+		printf("0");
+		return;
 	}
 
-	for (j = i - 1; j >= 0; j--)
-		_putchar(bin[j]);
+	while (n > 0)
+	{
+		bin += (n & 1) * base;
+		n >>= 1;
+		base *= 10;
+	}
+	printf("%d", bin);
+
 }
