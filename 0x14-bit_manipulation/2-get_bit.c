69e07adbdc670ecf1efd04b386ac1aa93b63f5b3
David <davifletcher2003@gmail.com>
2023-05-05 16:10:35 +0300

    get_bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the the value of index
+ * @index: position
+ *
+ * Return: value
+ *
+ */
+
+ int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int store;
+	if (index > 64)
+		return (-1);
+
+	store = n >> index;
+
+	return (store & 1);
+
+
+}
