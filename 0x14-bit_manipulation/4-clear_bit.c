c6275f8940cc28e98e1e50ce984c4afaa19c28d6
David <davifletcher2003@gmail.com>
2023-05-05 22:02:52 +0300

    set_bit_to_0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * clear_bit - clears bit
+ * @n: number argument
+ * @index: nth bit
+ * Return: cleared bit
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+
+	*n &= ~(1 << index);
+
+	return (*n >> index & 1);
+}
