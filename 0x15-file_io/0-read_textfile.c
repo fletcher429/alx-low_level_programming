68a1a421459c6432cbf6ec931b2a64d96a87bc00
David <davifletcher2003@gmail.com>
2023-05-09 00:52:54 +0300

    two

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,21 +7,18 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_dec;
-	int read_f;
-	int write_f;
-	char buffer[2048];
-
+	int file_dec; /*open the file*/
+	int read_f; /*read the file*/
+	int write_f; /*writ the file to the stout*/
+	char buffer[2048]; /*stores the letters*/
 	if (!filename)
 		return (0);
 
 	file_dec = open(filename, O_RDONLY);
-
 	if (file_dec < 0)
 		return (0);
 
 	read_f = read(file_dec, buffer, letters);
-
 	if (read_f < 0)
 		return (0);
 	buffer[read_f] = '\0';
@@ -30,8 +27,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (write_f < 0)
 		return (0);
 	return (write_f);
-
-
-
-
 }
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file
+ * @letter:  contents of the file
+ * Return: created_file
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	char *buf;
+	int file_name;
+    int write_file;
+    int read_file;
+
+
+	buf = malloc(strlen(text_content) + 1);
+
+	if (!buf)
+		return (0);
+
+	file_name = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file_name < 0)
+    {
+        free(buf);
+        return (0);
+
+    }
+
+    read_file = strlen(text_content);
+    strcpy(buf, text_content);
+
+    buf[read_file] = '\0';
+
+    write_file = write(file_name, buf, strlen(text_content));
+    free(buf);
+    if (write_file < 0)
+    {
+        close(file_name);
+        return (-1);
+    }
+    return (1);
+
+
+}
+
