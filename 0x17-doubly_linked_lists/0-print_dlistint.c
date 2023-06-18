996249cc1ba6f8f480164279f7f6e81436939cfc
David <davifletcher2003@gmail.com>
2023-06-18 19:15:17 +0300

    all

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_dlistint - prints a double linked list
+ * @h: head node
+ * Return: Count
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
